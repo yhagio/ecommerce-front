9a5ebdbaacb113c1dce54f6b8bc748800f885a55
Yuichi Hagio <yhagio87@gmail.com>
2016-11-08 15:31:17 -0500

    before

--- src/containers/AccountContainer.js
+++ src/containers/AccountContainer.js
@@ -1,12 +1,32 @@
 import React, { Component, PropTypes } from 'react';
+// import { bindActionCreators } from 'redux';
+// import { connect } from 'react-redux';
+
+// import * as accountActions from '../redux/Account';
 import Account from '../components/Account';
 
 class AccountContainer extends Component {
+  componentDidMount() {
+
+  }
+
   render() {
     return (
       <Account />
     )
   }
+}
+
+AccountContainer.propTypes = {
+
+}
+
+function mapStateToProps() {
+
+}
+
+function mapDispatchToProps() {
+  
 }
 
 export default AccountContainer;
